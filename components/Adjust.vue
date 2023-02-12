42b23eae1fc313f00bc66e0c9e6b98f28ce3cff6
许圣峰 <kricsleo@163.com>
2023-02-12 22:48:09 +0800

    feat(WIP): adjust plan

---
+++ components/Adjust.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useMousePressed } from '@vueuse/core';
+import { Plan } from '~~/composables/days';
+
+const props = defineProps<{plan: Plan}>()
+const adjustRef = ref<HTMLButtonElement>()
+const { pressed } = useMousePressed({ target: adjustRef })
+
+watch(pressed, () => {
+  setTimeout(() => {
+    if(pressed.value) {
+      // const newPlanId = planner.add(props.day.date, props.day.date, props.day.date)
+      // todo: set plan id
+      editingPlanId.value = props.plan.id
+    } else {
+      // contexted.value = false
+      editingPlanId.value = null
+    }
+  })
+})
+</script>
+
+<template>
+  <button ref="adjustRef" title="adjust" class="i-carbon:fit-to-width ml-auto" />
+</template>
