e0680217e232545ba7a4660bb31eeefdd4685e66
许圣峰 <kricsleo@163.com>
2023-02-22 14:09:29 +0800

    chore: update

---
+++ components/OptPanel.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import InputHours from './InputHours.vue'
+import { toggleDark } from '~/composables/dark';
+import { focusToday, hours, planner } from '~/composables/days'
+</script>
+
+<template>
+  <!-- <h1 text-center>Keep in mind: 996.icu</h1> -->
+  <div flex="~ col" gap-5 center text-2xl>
+    <button @click="focusToday" class="i-carbon:location-heart-filled" />
+    <InputHours v-model="hours" />
+    <button i-carbon:clean @click="planner.clean" />
+    <button @click="toggleDark()" class="dark:i-carbon-moon i-carbon-sun" />
+    <a
+      class="i-carbon-logo-github"
+      rel="noreferrer"
+      href="https://github.com/kricsleo/day"
+      target="_blank"
+      title="GitHub"
+    />
+  </div>
+</template>
